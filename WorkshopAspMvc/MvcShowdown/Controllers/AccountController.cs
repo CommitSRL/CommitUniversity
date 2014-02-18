f3b276f70d5c924fd7bea791074a38fdc3090b9d
Massimiliano Tavanti <max.tavanti@gmail.com>
2014-02-18 08:46:14 +0100

    added asp mvc workshop examples and code

---
+++ WorkshopAspMvc/MvcShowdown/Controllers/AccountController.cs
@@ -0,0 +1,28 @@
+﻿using MvcShowdown.Models.Account;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcShowdown.Controllers
+{
+    public class AccountController : Controller
+    {
+        //
+        // GET: /Account/
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(LoginViewModel model)
+        {
+            if(model.UserName=="andysal" && model.Password=="pippo")
+            {
+                return Redirect("/");
+            }
+            return View();
+        }
+	}
+}
---
+++ WorkshopAspMvc/MvcShowdown/Models/Account/RegisterViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcShowdown.Models.Account
+{
+    public class LoginViewModel
+    {
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public string Password { get; set; }
+
+    }
+}
